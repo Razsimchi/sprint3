bd30e21da7619e704835e7d49c6393ec07fc147d
Raz Simchi <razsimchi@gmail.com>
2023-05-13 18:51:15 +0300

    fix bug

--- apps/mail/cmps/mail-filter.jsx
+++ apps/mail/cmps/mail-filter.jsx
@@ -24,10 +24,10 @@ const navigate = useNavigate()
 
     return (
         <section className = 'mail-filter flex justify-center align-center column ' >
-            <button title = "Inbox" className="btn btn-inbox" name ="status" value ="inbox" onClick = {handleChange}><i className="fa-solid fa-inbox"></i></button>    
-            <button title = "Sent" className="btn btn-sent" name = "status" value ="sent" onClick = {handleChange}><i className="fa-regular fa-paper-plane"></i></button>    
-            <button title = "Trash" className="btn btn-trash"  name = "status" value ="trash" onClick = {handleChange}><i className="fa-regular fa-trash-can"></i></button>   
-            <button title = "Star" className="btn btn-str"  name = "isStared" value="star" onClick = {handleChange}><i className="fa-regular fa-star"></i></button>   
+            <button title = "Inbox" className="btn btn-filter btn-inbox" name ="status" value ="inbox" onClick = {handleChange}><i style={{ pointerEvents: 'none' }} className="fa-solid fa-inbox"></i></button>    
+            <button title = "Sent" className="btn btn-filter btn-sent" name = "status" value ="sent" onClick = {handleChange}><i style={{ pointerEvents: 'none' }} className="fa-regular fa-paper-plane"></i></button>    
+            <button title = "Trash" className="btn btn-filter btn-trash"  name = "status" value ="trash" onClick = {handleChange}><i style={{ pointerEvents: 'none' }} className="fa-regular fa-trash-can"></i></button>   
+            <button title = "Star" className="btn btn-filter btn-str"  name = "isStared" value="star" onClick = {handleChange}><i style={{ pointerEvents: 'none' }} className="fa-regular fa-star"></i></button>   
 
         </section>
     )
--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -1,6 +1,7 @@
 import { MailPreview } from "./mail-preview.jsx"
 export function MailList({mails , onRemoveMail ,  onStarOrMail}) {
 
+    if (mails.length === 0) return <h2>No emails yet</h2>
     return <table className = "mail-list" border="1">
         <tbody>
             {mails.map(mail => <MailPreview key={mail.id} mail={mail} onStarOrMail={onStarOrMail} onRemoveMail={onRemoveMail} />)}
